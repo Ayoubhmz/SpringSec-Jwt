8a8523b1b08bacace1276773790f7e322553730e
Ayoubhmz <92750888+Ayoubhmz@users.noreply.github.com>
2025-02-25 19:51:48 +0100

    updateAppConfig

---
+++ src/main/java/com/example/springsec/auth/AuthenticationController.java
@@ -0,0 +1,13 @@
+package com.example.springsec.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+
+public class AuthenticationController {
+
+}
