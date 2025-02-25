09b21ad8194291b8491c9dd119c6fe0484c82abf
Ayoubhmz <92750888+Ayoubhmz@users.noreply.github.com>
2025-02-25 16:44:57 +0100

    add Enum Role and UserRepo

---
+++ src/main/java/com/example/springsec/user/Role.java
@@ -0,0 +1,5 @@
+package com.example.springsec.user;
+
+public enum Role {
+    USER, ADMIN
+}
--- src/main/java/com/example/springsec/user/User.java
+++ src/main/java/com/example/springsec/user/User.java
@@ -1,14 +1,12 @@
 package com.example.springsec.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -28,16 +26,22 @@ public class User implements UserDetails {
     private String lastName;
     private String email;
     private String password;
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
     }
 
     @Override
