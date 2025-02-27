3bc0c65a7b8f25cca93d991a9948b1878d96ba45
Ayoubhmz <92750888+Ayoubhmz@users.noreply.github.com>
2025-02-27 22:26:27 +0100

    save the generated token

--- src/main/java/com/example/springsec/auth/AuthenticationService.java
+++ src/main/java/com/example/springsec/auth/AuthenticationService.java
@@ -2,6 +2,9 @@ package com.example.springsec.auth;
 
 import com.example.springsec.Repositories.UserRepository;
 import com.example.springsec.config.JwtService;
+import com.example.springsec.token.Token;
+import com.example.springsec.token.TokenRepository;
+import com.example.springsec.token.TokenType;
 import com.example.springsec.user.Role;
 import com.example.springsec.user.User;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
     private final UserRepository repository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -27,11 +31,23 @@ public class AuthenticationService {
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
                 .build();
-        repository.save(user);
+        var savedUser = repository.save(user);
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(savedUser, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .revoked(false)
+                .expired(false)
+                .build();
+        tokenRepository.save(token);
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -44,6 +60,7 @@ public class AuthenticationService {
         var user = repository.findByEmail(request.getEmail())
                 .orElseThrow();
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(user, jwtToken);
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
