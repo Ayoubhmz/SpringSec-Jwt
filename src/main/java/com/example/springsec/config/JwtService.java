7c91c9fd77d10dd5ff828e962325c41304d4c629
Ayoubhmz <92750888+Ayoubhmz@users.noreply.github.com>
2025-02-28 18:47:01 +0100

    Create Refresh Token methode

--- src/main/java/com/example/springsec/config/JwtService.java
+++ src/main/java/com/example/springsec/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,14 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "QJXCy0hs9+zVQZzoCAkIxQcdR7vqI1gFfcQylyp8haoHKgu7zrovKQtGEok5Obh+J4c44MWSZesi5x9wtjldJzHxUQ0Hzk0VcmMasG833Y6rp5OPLvohVvwCORgthA/Le7aX8tagtjRU8vQ5g9fBEi2bERVRCWZlHuVgHWOHpAg8A8yzfQ7QY7MyH/DUHRNd5JDSbHYfRGkZamcI33aSpH2BhBJC+ZqorjeCEwrdLgU0o4rseXburZudmZn2ageBTRlGGthNTIkR+d9m6EughuMM5nYqlUCw3YFtURn64KXjYn3m7OfXs43H6fyGuBIvOCOGkaqwGz+ToKG/e0svy+iEqPXVOLnndOTVIp3SMEc=";
+    @Value("${application.security.jwt.secret-key}")
+    private  String secretKey;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
 
     public String extractUserName(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -32,14 +40,27 @@ public class JwtService {
         return generateToken(new HashMap<>(0), userDetails);
     }
 
+    public String generateRefreshToken(
+                                UserDetails userDetails) {
+        return buildToken(new HashMap<>(), userDetails, refreshExpiration);
+
+    }
+
     public String generateToken(Map<String, Object> extraClaims,
                                 UserDetails userDetails) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+
+    }
+
+    private String buildToken(Map<String, Object> extraClaims,
+                              UserDetails userDetails,
+                              long expiration) {
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
 
@@ -68,7 +89,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
