c32c6e5dc5bee424997975d171064c4603a3abe1
Ayoubhmz <92750888+Ayoubhmz@users.noreply.github.com>
2025-03-02 14:30:01 +0100

    Roles and Permissions Based Authorization

--- src/main/java/com/example/springsec/SpringSecApplication.java
+++ src/main/java/com/example/springsec/SpringSecApplication.java
@@ -1,14 +1,47 @@
 package com.example.springsec;
 
+import com.example.springsec.auth.AuthenticationService;
+import com.example.springsec.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import static com.example.springsec.user.Role.ADMIN;
+import static com.example.springsec.user.Role.MANAGER;
 
 @SpringBootApplication
 public class SpringSecApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstName("Admin")
+                    .lastName("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+            var manager = RegisterRequest.builder()
+                    .firstName("Admin")
+                    .lastName("Admin")
+                    .email("manager@mail.com")
+                    .password("password")
+                    .role(MANAGER)
+                    .build();
+            System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+        };
     }
 
 }
