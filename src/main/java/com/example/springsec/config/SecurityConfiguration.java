52e43451712a0840f8d0a234cc52a1d926e005c9
Ayoubhmz <92750888+Ayoubhmz@users.noreply.github.com>
2025-03-02 17:15:51 +0100

    Upgrade Spring Security to Spring Boot 3.x and Crush Deprecations!

--- src/main/java/com/example/springsec/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsec/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,6 +19,7 @@ import static com.example.springsec.user.Permission.*;
 import static com.example.springsec.user.Role.ADMIN;
 import static com.example.springsec.user.Role.MANAGER;
 import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -32,41 +34,31 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                    req.requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
-
                 .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
                 .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
                 .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
                 .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
-
-//                .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
-//
-//                .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
-//                .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
-//                .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
-//                .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
-
-
-
                 .anyRequest()
                 .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout()
-                .logoutUrl("/api/v1/auth/logout")
-                .addLogoutHandler(logoutHandler)
-                .logoutSuccessHandler(
-                         (request, response, authentication) ->
-                        SecurityContextHolder.clearContext());
+                .logout(logout ->
+                        logout.logoutUrl("/api/v1/auth/logout")
+                              .addLogoutHandler(logoutHandler)
+                              .logoutSuccessHandler(
+                                    (request, response, authentication) ->
+                                            SecurityContextHolder.clearContext())
+
+                )
+                ;
+
 
         return http.build();
     }
